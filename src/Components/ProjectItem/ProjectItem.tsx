748f4527a42598f700c9fc59a8650a9867dae0a4
Austin Whitbeck <austinwhitbeckart@gmail.com>
2021-12-01 22:24:05 -0500

    adding images and data for the projects. Links working correctly. Separate art projects from Dev Projects

--- src/Components/ProjectItem/ProjectItem.tsx
+++ src/Components/ProjectItem/ProjectItem.tsx
@@ -1,3 +1,15 @@
+//**** Images imports ****//
+
+// SkedMan //
+import SkedMobile from '../../Images/Skedman_Mobile_img.png';
+import SkedMobile150px from '../../Images/Skedman_Mobile_150px.png';
+import SkedMobile300px from '../../Images/Skedman_Mobile_300px.png';
+
+// MinerMadness //
+import MinerMadness300px from '../../Images/MinerMadness_300px.png';
+
+// SocialNet //
+import SocialNet300px from '../../Images/SocialNet_300px.png';
 
 
 import './ProjectItem.css';
@@ -10,17 +22,40 @@ interface Props {
 
 }
 
-const ProjectItem = () => {
+
+
+const ProjectItem = ({link, image, title, blurb}: Props) => {
+
+    let thisImage = undefined;
+
+
+    switch (image) {
+        case image = 'SkedMan':
+            thisImage = SkedMobile
+            break;
+        case image = 'SkedMan150':
+            thisImage = SkedMobile150px
+            break;
+        case image = 'SkedMan300':
+            thisImage = SkedMobile300px  
+            break;
+        case image = 'MinerMadness300':
+            thisImage = MinerMadness300px
+            break;
+        case image = 'SocialNet300':
+            thisImage = SocialNet300px
+            break;
+    }
 
     return(
         <main className="projectItemContainer">
             <section className="projectPrevImgSect">
-                <img src="" alt="project preview picture" />
+                <img src={thisImage} alt="project preview picture" className="projectImage"/>
             </section>
             <section className="projectInfoSect">
-                <h4 className="projectTitle">Project Title</h4>
-                <p>Info about project</p>
-                <a href="">...Rerouting to Project...</a>
+                <h4 className="projectTitle">Project: {title}</h4>
+                <p>{blurb}</p>
+                <a href={link} target="_blank">...Rerouting to Project...</a>
             </section>
         </main>
 
--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -5,13 +5,36 @@ import './Projects.css';
 
 const Projects = () => {
 
+    const blurbs = [
+        // SkedMan
+        "SkedMan is a volunteer management system. I did most of this project",
+        // SocialNet
+        "SocialNet is a social media project utilizing pulling data from an API and displaying this by mapping the information across reusable components"
+
+    ]
+
     return(
         <main className="container allprojects">
             <h3 className="pageTitle">The Greaves: Projects</h3>
             <section className="projectItemsSect">
-                <ProjectItem/>
-                <ProjectItem/>
-                <ProjectItem/>
+                <ProjectItem
+                    link="https://my-scheduling-62f94.web.app/"
+                    image='SkedMan300'
+                    title="SkedMan"
+                    blurb={blurbs[0]}
+                />
+                <ProjectItem
+                    link="https://socialnet-bac93.web.app/"
+                    image="SocialNet300"
+                    title="SocialNet"
+                    blurb={blurbs[1]}
+                />
+                <h2>Art projects</h2>
+                <ProjectItem
+                    link="https://www.artstation.com/artwork/3PEkB"
+                    image="MinerMadness300"
+                    title="Miner Madness"
+                />
                 <ProjectItem/>
                 <ProjectItem/>
             </section>
